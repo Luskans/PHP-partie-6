8643812531dda0aa0dd0602b50abd17e1f9149c2
Luskans <sylvain.migne@gmail.com>
2023-06-29 11:14:47 +0200

    add exercices p6

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+///////////// EXERCICE 1 //////////////
+
+// if (isset($_GET['lastname']) && $_GET['lastname'] !== '') {
+//     if (isset($_GET['firstname']) && $_GET['firstname'] !== '') {
+//         echo 'Nom : '.$_GET['lastname'].'<br/>';
+//         echo 'Prénom : '.$_GET['firstname'];
+//     };
+// };
+
+///////////// EXERCICE 2 //////////////
+
+// if (isset($_GET['lastname']) && $_GET['lastname'] !== '') {
+//     if (isset($_GET['firstname']) && $_GET['firstname'] !== '') {
+//         echo 'Nom : '.$_GET['lastname'].'<br/>';
+//         echo 'Prénom : '.$_GET['firstname'];
+//         if (isset($_GET['age']) && $_GET['age'] !== '') {
+//             echo 'Age : '.$_GET['age'];
+//         } else {
+//             echo "<br/>Aucun âge n'est indiqué.";
+//         };
+//     };
+// };
+
+///////////// EXERCICE 3 //////////////
+
+// if (isset($_GET['startDate']) && $_GET['startDate'] !== '') {
+//     if (isset($_GET['endDate']) && $_GET['endDate'] !== '') {
+//         echo 'Date de début : '.$_GET['startDate'].'<br/>';
+//         echo 'Date de fin : '.$_GET['endDate'];
+//     };
+// };
+
+///////////// EXERCICE 4 //////////////
+
+// if (isset($_GET['language']) && $_GET['language'] !== '') {
+//     if (isset($_GET['server']) && $_GET['server'] !== '') {
+//         echo 'Langue : '.$_GET['language'].'<br/>';
+//         echo 'Serveur : '.$_GET['server'];
+//     };
+// };
+
+///////////// EXERCICE 5 //////////////
+
+// if (isset($_GET['week']) && $_GET['week'] !== '') {
+//     echo 'Semaine : '.$_GET['week'];
+// };
+
+///////////// EXERCICE 6 //////////////
+
+// if (isset($_GET['building']) && $_GET['building'] !== '') {
+//     if (isset($_GET['room']) && $_GET['room'] !== '') {
+//         echo 'Batiment : '.$_GET['building'].'<br/>';
+//         echo 'Chambre : '.$_GET['room'];
+//     };
+// };
+
+?>
